9638eb6ecab3c72c7e5cca753b7097bac84ddcf1
otavio <otaviotemoteo@gmail.com>
2025-04-09 11:18:50 -0300

    rfid test

---
+++ errors and tests/rfid/TestRfid/TestRfid.ino
@@ -0,0 +1,32 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();
+  rfid.PCD_Init();
+
+  Serial.println("Scan an RFID card...");
+}
+
+void loop() {
+  if (!rfid.PICC_IsNewCardPresent()) return;
+  if (!rfid.PICC_ReadCardSerial()) return;
+
+  Serial.print("Card UID: ");
+  for (byte i = 0; i < rfid.uid.size; i++) {
+    Serial.print(rfid.uid.uidByte[i] < 0x10 ? "0" : "");
+    Serial.print(rfid.uid.uidByte[i], HEX);
+  }
+  Serial.println();
+  
+  delay(1000);
+
+  rfid.PICC_HaltA();
+  rfid.PCD_StopCrypto1();
+}
